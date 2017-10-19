3fa963a34fd85d87f7e11646fa7444d8e5a9155b
Rasmus K. Kristiansen <rasmus@rasmus.kr>
2017-10-19 15:08:22 +0200

    Working Vim configuration
    Range of features added,
    
      - Fuzzy file, buffer and tag navigation (FZF)
      - IDE-like structure overview (NERDTree)
      - Short and modifier-free shortcuts (see <leader>)
    
    Successfully tested on Linux using Vim 8.

---
+++ Vim/install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Configures the directory structure, installs the Pathogen package
+# manager and the needed bundles.
+
+set -e
+
+BUNDLE_ROOT="${HOME}/.vim/bundle"
+AUTOLOAD_ROOT="${HOME}/.vim/autoload"
+PATHOGEN_VIM="${AUTOLOAD_ROOT}/pathogen.vim"
+
+if [ ! -d "${BUNDLE_ROOT}" ]; then
+    mkdir -p ${BUNDLE_ROOT}
+fi
+
+if [ ! -d "${AUTOLOAD_ROOT}" ]; then
+    mkdir -p ${AUTOLOAD_ROOT}
+fi
+
+if [ ! -f ${PATHOGEN_VIM} ]; then
+    curl -LSso ${PATHOGEN_VIM} https://tpo.pe/pathogen.vim
+fi
+
+
+# Fugitive
+if [ ! -d "${BUNDLE_ROOT}/vim-fugitive" ]; then
+    git clone https://github.com/tpope/vim-fugitive.git "${BUNDLE_ROOT}/vim-fugitive"
+fi
+
+# Tagbar
+if [ ! -d "${BUNDLE_ROOT}/tagbar" ]; then
+    git clone https://github.com/majutsushi/tagbar.git "${BUNDLE_ROOT}/tagbar"
+fi
+
+# Goyo
+if [ ! -d "${BUNDLE_ROOT}/goyo" ]; then
+    git clone https://github.com/junegunn/goyo.vim.git "${BUNDLE_ROOT}/goyo"
+fi
+
+# Vim Commentary
+if [ ! -d "${BUNDLE_ROOT}/vim-commentary" ]; then
+    git clone git://github.com/tpope/vim-commentary.git "${BUNDLE_ROOT}/vim-commentary"
+fi
+
+# Fuzzy Finder for Vim (FZF), requires FZF Github repo to
+# be checked out in ~/.fzf and ~/.fzf/install.sh being run beforehand.
+if [ ! -d "${BUNDLE_ROOT}/fzf" ]; then
+    git clone git://github.com/junegunn/fzf.vim "${BUNDLE_ROOT}/fzf"
+fi
+
+# Vim Go
+if [ ! -d "${BUNDLE_ROOT}/vim-go" ]; then
+    git clone git://github.com/fatih/vim-go.git "${BUNDLE_ROOT}/vim-go"
+fi
+
+# NERDTree
+if [ ! -d "${BUNDLE_ROOT}/nerdtree" ]; then
+    git clone git://github.com/scrooloose/nerdtree "${BUNDLE_ROOT}/nerdtree"
+fi
+
