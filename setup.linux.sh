1dbc6de0c3f2ce0e80c9de1f332a56725fc39feb
Rasmus K. Kristiansen <rasmus@rasmus.kr>
2017-11-26 18:11:27 +0100

    Install Golang fonts

--- setup.linux.sh
+++ setup.linux.sh
@@ -6,6 +6,7 @@
 #
 
 TMPDIR=${TMPDIR:-/tmp}
+FONT_ROOT="${HOME}/.local/share/fonts"
 
 apt_packages="tmux vim ruby git-core i3-wm i3-lock i3status mksh"
 apt_packages="${apt_packages} dmenu curl sakura"
@@ -31,6 +32,19 @@ if [ ! -d "${dvorarkk_dir}" ]; then
     mkdir -p "${dvorarkk_dir}"
     git clone https://github.com/rkk/Dvorarkk.git "${dvorarkk_dir}"
 fi
+
+# Golang fonts.
+if [ ! -d "${FONT_ROOT}" ]; then
+    mkdir -p "${FONT_ROOT}"
+fi
+font_tar="${TMPDIR}/ttfs.tar.gz"
+curl -sS "https://go.googlesource.com/image/+archive/master/font/gofont/ttfs.tar.gz" > "${font_tar}"
+tar xzf "${font_tar}" -C "${FONT_ROOT}"
+if [ -f "${font_tar}" ]; then
+    rm -f "${font_tar}"
+fi
+
+
 
 echo "Done."
 
