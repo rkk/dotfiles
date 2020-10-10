a9e8c2d9ebd4ca9308aa959ac604eeb892de7602
Rasmus K. Kristiansen <rasmus@rasmus.kr>
2020-10-10 11:14:44 +0200

    Vim: Switch color schemes and clean up file

--- .vim/install.sh
+++ .vim/install.sh
@@ -38,22 +38,18 @@ if [ ! -f "${PATHOGEN_VIM}" ]; then
 fi
 
 
-# Fugitive
 if [ ! -d "${BUNDLE_ROOT}/vim-fugitive" ]; then
     git clone git://github.com/tpope/vim-fugitive.git "${BUNDLE_ROOT}/vim-fugitive"
 fi
 
-# Tagbar
 if [ ! -d "${BUNDLE_ROOT}/tagbar" ]; then
     git clone git://github.com/majutsushi/tagbar.git "${BUNDLE_ROOT}/tagbar"
 fi
 
-# Goyo
 if [ ! -d "${BUNDLE_ROOT}/goyo" ]; then
     git clone git://github.com/junegunn/goyo.vim.git "${BUNDLE_ROOT}/goyo"
 fi
 
-# Vim Commentary
 if [ ! -d "${BUNDLE_ROOT}/vim-commentary" ]; then
     git clone git://github.com/tpope/vim-commentary.git "${BUNDLE_ROOT}/vim-commentary"
 fi
@@ -64,33 +60,27 @@ if [ ! -d "${BUNDLE_ROOT}/fzf" ]; then
     git clone git://github.com/junegunn/fzf.vim "${BUNDLE_ROOT}/fzf"
 fi
 
-# Vim Go
 if [ ! -d "${BUNDLE_ROOT}/vim-go" ]; then
     git clone git://github.com/fatih/vim-go.git "${BUNDLE_ROOT}/vim-go"
 fi
 
-# NERDTree
 if [ ! -d "${BUNDLE_ROOT}/nerdtree" ]; then
     git clone git://github.com/scrooloose/nerdtree "${BUNDLE_ROOT}/nerdtree"
 fi
 
-# Git Gutter
 if [ ! -d "${BUNDLE_ROOT}/vim-gitgutter" ]; then
     git clone git://github.com/airblade/vim-gitgutter.git "${BUNDLE_ROOT}/vim-gitgutter"
 fi
 
-# Solarized
-if [ ! -d "${BUNDLE_ROOT}/vim-colors-solarized" ]; then
-    git clone git://github.com/altercation/vim-colors-solarized "${BUNDLE_ROOT}/vim-colors-solarized"
-fi
-
-# Syntastic
 if [ ! -d "${BUNDLE_ROOT}/syntastic" ]; then
     git clone --depth=1 git://github.com/vim-syntastic/syntastic.git "${BUNDLE_ROOT}/syntastic"
 fi
 
-# Supertab
 if [ ! -d "${BUNDLE_ROOT}/supertab" ]; then
     git clone git://github.com/ervandew/supertab.git "${BUNDLE_ROOT}/supertab"
+fi
+
+if [ ! -d "${BUNDLE_ROOT}/porter" ]; then
+    git clone git://github.com/rkk/porter.git "${BUNDLE_ROOT}/porter"
 fi
 
