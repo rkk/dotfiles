27c0d671a246c9a671fee711eb394ba242fceb42
Rasmus K. Kristiansen <rasmus@rasmus.kr>
2020-10-11 11:35:22 +0200

    bin: Fix Shellcheck warnings and clean up

--- bin/vpn.sh
+++ bin/vpn.sh
@@ -4,19 +4,28 @@
 #
 
 openvpn="/usr/local/sbin/openvpn"
+openvpn_alt="/usr/sbin/openpvn"
 connection="/etc/openvpn/default.conf"
 
 if [ ! -f "${openvpn}" ]; then
-    echo "ERROR: OpenVPN not installed at ${openvpn}"
-    exit 1
+	if [ ! -f "${openvpn_alt}" ]; then
+        echo "ERROR: OpenVPN not installed at ${openvpn} and neither at ${openvpn_alt}"
+    	exit 1
+    fi
+    openvpn="${openvpn_alt}"
 fi
 
 if [ ! -f "${connection}" ]; then
     echo "ERROR: Configuration file not found: ${connection}"
-    exit 2
+    exit 1
 fi
 
 if [ $(uname -s) = "OpenBSD" ]; then
     doas ${openvpn} --config ${connection}
 fi
+
+if [ $(uname -s) = "Linux" ]; then
+	sudo "${openvpn}" --config "${connection}"
+fi
+
 
